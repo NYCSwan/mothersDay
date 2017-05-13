7e37e7803f6604cdde2fc8d83bd3d5efd962baae
NYCSwan <megan.swanby@gmail.com>
2017-05-13 10:26:56 -0400

    Delete home page file, fixed paths for public files

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
-// app.use(express.static(path.join(__dirname + "./public")));
 app.use(express.static('public'));
 app.use(cors());
 app.use(routes);
@@ -15,6 +14,7 @@ app.use(routes);
 app.engine('html', require('ejs').renderFile);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
+
 app.use(function(req, res, next) {
   console.log(`${req.method} request for ${req.url}`);
   next();
