c8399a928732d41f898194be0cc8d37ee7afdf03
NYCSwan <megan.swanby@gmail.com>
2017-05-13 10:14:26 -0400

    Add routes folder and index route file, added ejs for the views. Set views to html.

--- app.js
+++ app.js
@@ -1,22 +1,45 @@
 const express = require('express');
 const cors = require('cors');
-
+const routes = require('./routes/');
+const path = require('path');
 const app = express();
 
-const port = port.env.PORT || 3000;
+app.set('port', (process.env.PORT || 3000));
 
+// app.use(express.static(path.join(__dirname + "./public")));
+app.use(express.static('public'));
+app.use(cors());
+app.use(routes);
 
+// view engine setup
+app.engine('html', require('ejs').renderFile);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
 app.use(function(req, res, next) {
   console.log(`${req.method} request for ${req.url}`);
   next();
 });
 
-app.use(express.static(__dirname + "./public"));
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-app.use(cors());
+app.get('/', function(req, res){
+  res.render('index', function(err, html) {
+    if(err) {
+      console.error(err);
+    } else {
+      console.log(html);
+      res.send(html);
+    }
+  });
+});
 
-app.listen(port, function() {
-  console.log("Express app running on port 3000");
+app.listen(app.get('port'), function() {
+  console.log("Express app running on port ", app.get('port'));
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const http = require('http');
-// node server
-
-function onRequest(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('hey');
-  res.end();
-}
-
-http.createServer(onRequest).listen(3000)
-
-console.log("listening on port 3000")
+// const http = require('http');
+// // node server
+//
+// function onRequest(req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.write('hey');
+//   res.end();
+// }
+//
+// http.createServer(onRequest).listen(3000)
+//
+// console.log("listening on port 3000")
